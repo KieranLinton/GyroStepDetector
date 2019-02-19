05862353ad2d45b16f30c898fa09422285a81931
soggyDoughnut <proton5@live.com>
2019-02-19 20:57:46 +0100

    a friend fixed some code for me, i have to tweak it a little

--- code/MainWindow.xaml.cs
+++ code/MainWindow.xaml.cs
@@ -1,119 +1,135 @@
-﻿namespace test
-{
-    using System.Diagnostics;
-    using System.IO.Ports;
-    using System.Windows;
-    using WindowsInput;
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Windows;
+using WindowsInput;
 
+namespace test
+{
+    ///  put this in another file
+    ///////////////////
+    // you would port this service out to as wee so other service could use it keep it here for now
+
+    public class AppLoggerService
+    {
+        public void SendToLog()
+        {
+            Debug.Write("Sent");
+        }
+    }
+    public class AccelleroService
+    {
+        private readonly AppLoggerService _appLogger = new AppLoggerService();
+
+        public ACCELERO UpdateParameterMath(int paramd, ACCELERO accelero)
+        {
+            if (accelero.value > (accelero.centerCords + accelero.paramd))
+            {
+                accelero.valOverParam = true;
+                accelero.valUnderParam = false;
+                accelero.inCenter = false;
+                Debug.Write("over!!  ");
+            }
+            if (accelero.value < (accelero.centerCords - accelero.paramd))
+            {
+                accelero.valOverParam = false;
+                accelero.valUnderParam = true;
+                accelero.inCenter = false;
+                Debug.Write("undrr!!  ");
+            }
+            if (accelero.value < (accelero.centerCords + accelero.paramd) && accelero.value > (accelero.centerCords - accelero.paramd))
+            {
+                accelero.valOverParam = false;
+                accelero.valUnderParam = false;
+                accelero.inCenter = true;
+                Debug.Write("center!!  ");
+            }
+            if (accelero.valOverParam && accelero.goingUp == false && accelero.goingDown == false && accelero.UpsRebound == false && accelero.DownsRebound == false)
+            {
+                accelero.goingUp = true;
+                accelero.up = true;
+                _appLogger.sendToLog("yooo");
+            }
+            if (accelero.valUnderParam && accelero.goingUp == false && accelero.goingDown == false && accelero.UpsRebound == false && accelero.DownsRebound == false)
+            {
+                accelero.goingDown = true;
+                accelero.down = true;
+            }
+            if (accelero.valUnderParam && accelero.goingUp == true)
+            {
+                accelero.goingUp = false;
+                accelero.UpsRebound = true;
+            }
+            if (accelero.valOverParam && accelero.goingDown == true)
+            {
+                accelero.goingDown = false;
+                accelero.DownsRebound = true;
+            }
+            if (accelero.inCenter && accelero.UpsRebound == true)
+            {
+                accelero.UpsRebound = false;
+                accelero.up = false;
+            }
+            if (accelero.inCenter && accelero.DownsRebound == true)
+            {
+                accelero.DownsRebound = false;
+                accelero.down = false;
+            }
+            return accelero;
+        }
+        public ACCELERO RESET(ACCELERO accelero)
+        {
+            accelero.up = false;
+            accelero.down = false;
+            accelero.goingUp = false;
+            accelero.UpsRebound = false;
+            accelero.goingDown = false;
+            accelero.DownsRebound = false;
+            return accelero;
+        }
+    }
+    public class ACCELERO
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+        public int centerCords { get; set; }
+        public bool valOverParam { get; set; }
+        public bool valUnderParam { get; set; }
+        public bool inCenter { get; set; }
+        public bool up { get; set; }
+        public bool down { get; set; }
+        public bool goingUp { get; set; }
+        public bool UpsRebound { get; set; }
+        public bool goingDown { get; set; }
+        public bool DownsRebound { get; set; }
+        //constructor
+        public ACCELERO()
+        {
+            value = 0;
+            centerCords = 0;
+            valOverParam = false;
+            valUnderParam = false;
+            inCenter = true;
+            up = false;
+            down = false;
+            goingUp = false;
+            UpsRebound = false;
+            goingDown = false;
+            DownsRebound = false;
+        }
+    }
+}
+///  END OF put this in another file
+namespace test
+{
     public partial class MainWindow : Window
     {
-        public SerialPort SP = new SerialPort();
-
-        internal InputSimulator sim = new InputSimulator();//for the keyboard
-
-        internal new ACCELERO AX1 = new ACCELERO("accelerometer1");
-        internal new ACCELERO AX2 = new ACCELERO("accelerometer2");
-        internal new ACCELERO AX3 = new ACCELERO("accelerometer3");
-
+        private SerialPort SP = new SerialPort();
+        InputSimulator sim = new InputSimulator();//for the keyboard
+        private AccelleroService accelleroService = new AccelleroService();
+        private ACCELERO AX1 = new ACCELERO("accelerometer1");
+        private ACCELERO AX2 = new ACCELERO("accelerometer2");
+        private ACCELERO AX3 = new ACCELERO("accelerometer3");
         public int param = 20;
-
-        internal class ACCELERO
-        {
-            public string name { get; set; }
-            //constructor
-            public ACCELERO(string _name)
-            {
-                name = _name;
-            }
-            public int value = 0;
-            public int centerCords = 0;
-
-            public bool valOverParam = false;
-            public bool valUnderParam = false;
-            public bool inCenter = true;
-
-            public bool up = false;
-            public bool down = false;
-
-            public bool goingUp = false;
-            public bool UpsRebound = false;
-            public bool goingDown = false;
-            public bool DownsRebound = false;
-
-
-            public void UpdateParameterMath(int paramd)
-            {
-
-                if (value > (centerCords + paramd))
-                {
-                    valOverParam = true;
-                    valUnderParam = false;
-                    inCenter = false;
-
-
-                    Debug.Write("over!!  ");
-                }
-
-                if (value < (centerCords - paramd))
-                {
-                    valOverParam = false;
-                    valUnderParam = true;
-                    inCenter = false;
-                    Debug.Write("undrr!!  ");
-                }
-                if (value < (centerCords + paramd) && value > (centerCords - paramd))
-                {
-                    valOverParam = false;
-                    valUnderParam = false;
-                    inCenter = true;
-                    Debug.Write("center!!  ");
-                }
-
-                if (valOverParam && goingUp == false && goingDown == false && UpsRebound == false && DownsRebound == false)
-                {
-                    goingUp = true;
-                    up = true;
-                }
-                if (valUnderParam && goingUp == false && goingDown == false && UpsRebound == false && DownsRebound == false)
-                {
-                    goingDown = true;
-                    down = true;
-                }
-                if (valUnderParam && goingUp == true)
-                {
-                    goingUp = false;
-                    UpsRebound = true;
-                }
-                if (valOverParam && goingDown == true)
-                {
-                    goingDown = false;
-                    DownsRebound = true;
-                }
-                if (inCenter && UpsRebound == true)
-                {
-                    UpsRebound = false;
-                    up = false;
-                }
-                if (inCenter && DownsRebound == true)
-                {
-                    DownsRebound = false;
-                    down = false;
-                }
-            }
-
-            internal void RESET()
-            {
-
-                up = false;
-                down = false;
-
-                goingUp = false;
-                UpsRebound = false;
-                goingDown = false;
-                DownsRebound = false;
-            }
-        }
-
         public MainWindow()
         {
             SP.PortName = "COM3";//Set your board COM
@@ -121,28 +137,22 @@
             SP.DataReceived += SP_DataReceived;
             SP.Open();
         }
-
         private void SP_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             // sim.Keyboard.KeyDown(VirtualKeyCode.VK_W);
-
             Debug.WriteLine(SP.ReadLine());
             string[] inputList = SP.ReadLine().Split(',');
-
             AX1.value = int.Parse(inputList[0]);
             AX2.value = int.Parse(inputList[1]);
             AX3.value = int.Parse(inputList[2]);
-
             //GUI elements
             this.Dispatcher.Invoke(() =>
             {
                 bar1.Value = AX1.value;
                 bar2.Value = AX2.value;
                 bar3.Value = AX3.value;
-
                 param = (int)paramVAL.Value;
                 val.Content = param;
-
                 //setting param tick things
                 Debug.Write("  " + AX1.valOverParam);
                 filterup1.IsChecked = AX1.valOverParam;
@@ -151,60 +161,49 @@
                 filterDown1.IsChecked = AX1.valUnderParam;
                 filterDown2.IsChecked = AX2.valUnderParam;
                 filterDown3.IsChecked = AX3.valUnderParam;
-
-
             });
             //accelorometer ALGO......
             {
-                AX1.UpdateParameterMath(param);
-                AX2.UpdateParameterMath(param);
-                AX3.UpdateParameterMath(param);
-
+                ///// changed herer
+                AX1 = accelleroService.UpdateParameterMath(param, AX1);
+                AX2 = accelleroService.UpdateParameterMath(param, AX2);
+                AX3 = accelleroService.UpdateParameterMath(param, AX3);
                 Debug.Write(AX1.up + " " + AX1.down);
-
             }
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             AX1.centerCords = AX1.value;
             AX2.centerCords = AX2.value;
             AX3.centerCords = AX3.value;
-
             sendToLog("center cords have been set to " + AX1.centerCords.ToString() + ", " + AX2.centerCords.ToString() + "  and  " + AX3.centerCords.ToString());
         }
-
-        private void sendToLog(string message)
+        public void sendToLog(string message)
         {
             logBOX.AppendText(System.Environment.NewLine + "#" + message);
             logBOX.ScrollToEnd();
         }
-
         private void KeyBoardEnabe_Click(object sender, RoutedEventArgs e)
         {
         }
-
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
         }
-
         private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
         {
         }
-
         private void CheckBox_Checked_2(object sender, RoutedEventArgs e)
         {
         }
-
         private void Bar1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
         }
-
         private void Reset_step_Click(object sender, RoutedEventArgs e)
         {
-            AX1.RESET();
-            AX2.RESET();
-            AX3.RESET();
+            ///// changed herer
+            AX1 = accelleroService.RESET(AX1);
+            AX2 = accelleroService.RESET(AX2);
+            AX3 = accelleroService.RESET(AX3);
         }
     }
 }
