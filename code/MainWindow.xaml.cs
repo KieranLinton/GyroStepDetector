0e81df37a9cc0d879625b335720de0b9a340b811
soggyDoughnut <proton5@live.com>
2019-02-20 13:03:28 +0100

    Added AXIS ro Send to log comunication and added com selection

--- code/MainWindow.xaml.cs
+++ code/MainWindow.xaml.cs
@@ -8,7 +8,6 @@
     public partial class MainWindow : Window
     {
         public SerialPort SP = new SerialPort();
-
         internal InputSimulator sim = new InputSimulator();//for the keyboard
 
         internal new ACCELERO AX1 = new ACCELERO("accelerometer1");
@@ -16,6 +15,8 @@
         internal new ACCELERO AX3 = new ACCELERO("accelerometer3");
 
         public int param = 20;
+
+        public static string toLog = null;
 
         internal class ACCELERO
         {
@@ -27,6 +28,7 @@
             }
             public int value = 0;
             public int centerCords = 0;
+            public bool logEnabled = true;
 
             public bool valOverParam = false;
             public bool valUnderParam = false;
@@ -39,7 +41,6 @@
             public bool UpsRebound = false;
             public bool goingDown = false;
             public bool DownsRebound = false;
-
 
             public void UpdateParameterMath(int paramd)
             {
@@ -73,11 +74,13 @@
                 {
                     goingUp = true;
                     up = true;
+                    if(logEnabled){toLog = name + " is going up";}
                 }
                 if (valUnderParam && goingUp == false && goingDown == false && UpsRebound == false && DownsRebound == false)
                 {
                     goingDown = true;
                     down = true;
+                    if (logEnabled) { toLog = name + " is going down"; }
                 }
                 if (valUnderParam && goingUp == true)
                 {
@@ -93,11 +96,13 @@
                 {
                     UpsRebound = false;
                     up = false;
+                    if (logEnabled) { toLog = name + " has finished going up"; }
                 }
                 if (inCenter && DownsRebound == true)
                 {
                     DownsRebound = false;
                     down = false;
+                    if (logEnabled) { toLog = name + " has finished going down"; }
                 }
             }
 
@@ -116,10 +121,8 @@
 
         public MainWindow()
         {
-            SP.PortName = "COM3";//Set your board COM
-            SP.BaudRate = 9600;
-            SP.DataReceived += SP_DataReceived;
-            SP.Open();
+           
+            
         }
 
         private void SP_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -128,32 +131,41 @@
 
             Debug.WriteLine(SP.ReadLine());
             string[] inputList = SP.ReadLine().Split(',');
-
-            AX1.value = int.Parse(inputList[0]);
-            AX2.value = int.Parse(inputList[1]);
-            AX3.value = int.Parse(inputList[2]);
+            try
+            {
+                AX1.value = int.Parse(inputList[0]);
+                AX2.value = int.Parse(inputList[1]);
+                AX3.value = int.Parse(inputList[2]);
+            }
+            catch
+            {
+                sendToLog("Error reading cereal values");
+            }
 
             //GUI elements
-            this.Dispatcher.Invoke(() =>
             {
-                bar1.Value = AX1.value;
-                bar2.Value = AX2.value;
-                bar3.Value = AX3.value;
+                this.Dispatcher.Invoke(() =>
+                {
+                    bar1.Value = AX1.value;
+                    bar2.Value = AX2.value;
+                    bar3.Value = AX3.value;
 
-                param = (int)paramVAL.Value;
-                val.Content = param;
+                    param = (int)paramVAL.Value;
+                    val.Content = param;
 
-                //setting param tick things
-                Debug.Write("  " + AX1.valOverParam);
-                filterup1.IsChecked = AX1.valOverParam;
-                filterup2.IsChecked = AX2.valOverParam;
-                filterup3.IsChecked = AX3.valOverParam;
-                filterDown1.IsChecked = AX1.valUnderParam;
-                filterDown2.IsChecked = AX2.valUnderParam;
-                filterDown3.IsChecked = AX3.valUnderParam;
+                    //setting param tick things
+                     Debug.Write("  " + AX1.valOverParam);
+                    filterup1.IsChecked = AX1.valOverParam;
+                    filterup2.IsChecked = AX2.valOverParam;
+                    filterup3.IsChecked = AX3.valOverParam;
+                    filterDown1.IsChecked = AX1.valUnderParam;
+                    filterDown2.IsChecked = AX2.valUnderParam;
+                    filterDown3.IsChecked = AX3.valUnderParam;
 
+                    
 
-            });
+                });
+            }
             //accelorometer ALGO......
             {
                 AX1.UpdateParameterMath(param);
@@ -162,6 +174,15 @@
 
                 Debug.Write(AX1.up + " " + AX1.down);
 
+            }
+
+            //some debug stuff
+            {
+                if (toLog != null)
+                {
+                    sendToLog(toLog);
+                    toLog = null;
+                }
             }
         }
 
@@ -173,38 +194,80 @@
 
             sendToLog("center cords have been set to " + AX1.centerCords.ToString() + ", " + AX2.centerCords.ToString() + "  and  " + AX3.centerCords.ToString());
         }
-
         private void sendToLog(string message)
         {
-            logBOX.AppendText(System.Environment.NewLine + "#" + message);
-            logBOX.ScrollToEnd();
-        }
+            this.Dispatcher.Invoke(() =>
+            {
+                logBOX.AppendText(System.Environment.NewLine + System.Environment.NewLine + "#" + message);
+                logBOX.ScrollToEnd();
 
+                //enabling/disabling LOG from accelerometers--
+                {
+                    AX1.logEnabled = AX1check.IsChecked.GetValueOrDefault();
+                    AX2.logEnabled = AX2check.IsChecked.GetValueOrDefault();
+                    AX3.logEnabled = AX3check.IsChecked.GetValueOrDefault();
+                }
+            });
+        }
         private void KeyBoardEnabe_Click(object sender, RoutedEventArgs e)
         {
         }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void CheckBox_Checked_2(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void Bar1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-        }
-
         private void Reset_step_Click(object sender, RoutedEventArgs e)
         {
             AX1.RESET();
             AX2.RESET();
             AX3.RESET();
+            sendToLog("Reseted all current ALGO values for all axis");
         }
+        private void COMguiList_Loaded(object sender, RoutedEventArgs e)
+        {
+            //setting the dropDown COM list....
+            this.Dispatcher.Invoke(() =>
+            {
+                string[] AvailableCOMS = SerialPort.GetPortNames();
+                for (int i = 0; i < AvailableCOMS.Length; i++)
+                {
+                    COMguiList.Items.Insert(i, AvailableCOMS[i]);
+                }
+            });
+        }
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                logBOX.Text = " ";
+                
+            });
+        }
+
+        private void Enable_COM_Click(object sender, RoutedEventArgs e)
+        {
+           
+            SP.BaudRate = 9600;
+            SP.DataReceived += SP_DataReceived;
+            try {
+                SP.PortName = COMguiList.SelectedValue.ToString();
+                SP.Open();
+                sendToLog("Connected :)");
+            } catch {
+                sendToLog("could not connect to COM");
+            }
+            
+
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+        }
+        private void CheckBox_Checked_2(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Bar1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        }
+
+        
     }
 }
